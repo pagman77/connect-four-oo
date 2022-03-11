1ff1c405dd50c7a5f5d30283edf4faaed1958a8c
Michael Paglione <pagman777@gmail.com>
2022-03-10 20:15:32 -0500

    fully functioning game, swapped this keyword in for OO programing

--- connect4.js
+++ connect4.js
@@ -9,11 +9,11 @@ class Game {
 
   constructor(height = 6, width = 7) {
     this.height = height,
-      this.width = width,
-      this.board = [],
-      this.currPlayer = 1
-      this.makeBoard();
-      this.makeHtmlBoard();
+    this.width = width,
+    this.board = [],
+    this.currPlayer = 1;
+    this.makeBoard();
+    this.makeHtmlBoard();
   }
 
   /** makeBoard: create in-JS board structure:
@@ -73,7 +73,7 @@ class Game {
   placeInTable(y, x) {
     const piece = document.createElement('div');
     piece.classList.add('piece');
-    piece.classList.add(`p${currPlayer}`);
+    piece.classList.add(`p${this.currPlayer}`);
     piece.style.top = -50 * (y + 2);
 
     const spot = document.getElementById(`${y}-${x}`);
@@ -94,31 +94,36 @@ class Game {
     const x = +evt.target.id;
 
     // get next spot in column (if none, ignore click)
-    const y = findSpotForCol(x);
+    const y = this.findSpotForCol(x);
     if (y === null) {
       return;
     }
 
     // place piece in board and add to HTML table
-    board[y][x] = currPlayer;
-    placeInTable(y, x);
+    this.board[y][x] = this.currPlayer;
+    this.placeInTable(y, x);
 
     // check for win
-    if (checkForWin()) {
-      return endGame(`Player ${currPlayer} won!`);
+    if (this.checkForWin()) {
+      return this.endGame(`Player ${this.currPlayer} won!`);
     }
 
     // check for tie
-    if (board.every(row => row.every(cell => cell))) {
-      return endGame('Tie!');
+    if (this.board.every(row => row.every(cell => cell))) {
+      return this.endGame('Tie!');
     }
 
     // switch players
-    currPlayer = currPlayer === 1 ? 2 : 1;
+    this.currPlayer = this.currPlayer === 1 ? 2 : 1;
   }
 
   /** checkForWin: check board cell-by-cell for "does a win start here?" */
   checkForWin() {
+
+    let height = this.height;
+    let board = this.board;
+    let currPlayer = this.currPlayer;
+
     function _win(cells) {
       // Check four cells to see if they're all color of current player
       //  - cells: list of four (y, x) cells
@@ -127,15 +132,15 @@ class Game {
       return cells.every(
         ([y, x]) =>
           y >= 0 &&
-          y < this.height &&
+          y < height &&
           x >= 0 &&
-          x < this.height &&
+          x < height &&
           board[y][x] === currPlayer
       );
     }
 
-    for (let y = 0; y < this.height; y++) {
-      for (let x = 0; x < this.height; x++) {
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < height; x++) {
         // get "check list" of 4 cells (starting here) for each of the different
         // ways to win
         const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
